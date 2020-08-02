64a1bfc2c78717f274756baff3e5e44c1961a888
Luis Acosta <luije87@gmail.com>
2020-08-01 20:19:59 -0400

    express init

---
+++ server/src/api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const emojis = require('./emojis');
+const players = require('./players');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - 👋🌎🌍🌏'
+  });
+});
+
+router.use('/emojis', emojis);
+router.use('/players', players);
+
+module.exports = router;
